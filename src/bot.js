6a117333c75abb838b782b737accc69281d62199
viz658 <emmanuelmarcial91@yahoo.com>
2024-01-26 16:23:31 -0800

    added gpt ai

--- src/bot.js
+++ src/bot.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 
-const { token, databaseToken } = process.env;
+const { token, databaseToken, OPENAI_KEY } = process.env;
 const { connect } = require("mongoose");
 const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const { OpenAI } = require("openai");
 const fs = require("fs");
 
+//main client
 const client = new Client({
   //32767 for all intents
   intents: [
@@ -41,5 +43,83 @@ connect(databaseToken).catch(console.error);
   await connect(databaseToken).catch(console.error);
 })();
 
-// youtube_id = UCa78NuGZqaApCcvGtsbVsLw
-// https://www.youtube.com/feeds/videos.xml?channel_id=UCa78NuGZqaApCcvGtsbVsLw
+//GPT BOT-premium/private only
+const CHANNELS = ["1200583320325603409"]; //channel ids array
+
+const openai = new OpenAI({
+  apiKey: OPENAI_KEY,
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+  if (
+    !CHANNELS.includes(message.channelId) &&
+    !message.mentions.users.has(client.user.id)
+  )
+    return;
+
+  await message.channel.sendTyping();
+
+  const sendTypingInterval = setInterval(() => {
+    message.channel.sendTyping();
+  }, 5000);
+
+  let conversation = [];
+
+  conversation.push({
+    role: "system",
+    content: "Chat GPT is a friendly chatbot.",
+  });
+
+  let prevMessages = await message.channel.messages.fetch({ limit: 10 });
+  prevMessages.reverse();
+
+  prevMessages.forEach((msg) => {
+    if (msg.author.bot && msg.author.id !== client.user.id) return;
+
+    const username = msg.author.username
+      .replace(/\s+/g, "_")
+      .replace(/[^\w\s]/gi, "");
+
+    if (msg.author.id === client.user.id) {
+      conversation.push({
+        role: "assistant",
+        name: username,
+        content: msg.content,
+      });
+
+      return;
+    }
+
+    conversation.push({
+      role: "user",
+      name: username,
+      content: msg.content,
+    });
+  });
+
+  const response = await openai.chat.completions
+    .create({
+      model: "gpt-3.5-turbo",
+      messages: conversation,
+    })
+    .catch((error) => console.error("OpenAI Error:\n", error));
+
+  clearInterval(sendTypingInterval);
+
+  if (!response) {
+    message.reply(
+      "I'm having some trouble with the OpenAI API. Try again in a moment."
+    );
+    return;
+  }
+
+  const responseMessage = response.choices[0].message.content;
+  const chunkSizeLimit = 2000;
+
+  for (let i = 0; i < responseMessage.length; i += chunkSizeLimit) {
+    const chunk = responseMessage.substring(i, i + chunkSizeLimit);
+
+    await message.reply(chunk);
+  }
+});
